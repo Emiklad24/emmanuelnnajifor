4dd2e88e16b072ab2e20d901e56abbd2b84ab7c9
EmmanuelChinonye Nnajiofor <emiklad1993@gmail.com>
2021-09-06 18:20:19 +0100

    initialised

---
+++ constants/data.js
@@ -0,0 +1,88 @@
+const userData = {
+  githubUsername: "Emiklad24",
+  name: "Emmanuel Nnajiofor",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.jpg",
+  email: "emma.nnajichi@gmail.com",
+  phone: "+234 8163785517 ",
+  address: "Abuja, Nigeria.",
+  projects: [
+    {
+      title: "Reporta Health",
+      link: "https://reportahealth.org/",
+      imgUrl: "/Reporta.png",
+    },
+    {
+      title: "Tetelastai Trading Company",
+      link: "https://ttcorporation.org/",
+      imgUrl: "/tetelastai.png",
+    },
+    {
+      title: "Moniya",
+      link: "https://moniya.com.ng/",
+      imgUrl: "/moniya.png",
+    },
+    {
+      title: "Movie app",
+      link: "https://movieapp-149cd.web.app/",
+      imgUrl: "/movieapp.png",
+    },
+    {
+      title: "World ranks",
+      link: "https://world-rankings-app.vercel.app/",
+      imgUrl: "/worldrank.png",
+    },
+    {
+      title: "REAPS",
+      link: "https://reaps.com.ng/",
+      imgUrl: "/REAPS.png",
+    },
+    {
+      title: "Disease Registry",
+      link: "#",
+      imgUrl: "/DISREG.png",
+    },
+
+    {
+      title: "Photo Store",
+      link: "https://bejamasshop.netlify.app/",
+      imgUrl: "/photostore.png",
+    },
+  ],
+  about: {
+    title: `Lorem ipsum dolor sit amet,
+      consectetur adipisicing elit. Molestias voluptates asperiores`,
+    description: [
+      `Lorem ipsum dolor sit amet consectetur adipisicing elit. At laborum et, beatae impedit dolores nesciunt ab vero aliquid totam quod ea quos facere in earum amet eligendi omnis quas. Aspernatur distinctio voluptate eum rerum deleniti reiciendis laborum dignissimos nisi tempore repudiandae, quaerat commodi expedita nobis. Lorem ipsum dolor sit amet consectetur adipisicing elit. At laborum et, beatae impedit dolores nesciunt ab vero aliquid totam quod ea quos facere in earum amet eligendi omnis quas. Aspernatur distinctio voluptate eum rerum deleniti reiciendis laborum dignissimos nisi tempore repudiandae, quaerat commodi expedita nobis.`,
+    ],
+    currentProject: "Secret Project 🤐",
+    currentProjectUrl: "#",
+  },
+  experience: [
+    {
+      title: "Web Developer",
+      company: "Viable knowledge Masters",
+      year: "2020",
+      companyLink: "https://www.viableknowledgemasters.com/",
+      desc: "Voluptate eum rerum deleniti reiciendis laborum dignissimos nisi tempore repudiandae, quaerat commodi expedita nobis",
+    },
+    {
+      title: "Web Developer",
+      company: "Licorne Technologies",
+      year: "2018",
+      companyLink: "https://licorne.com.ng/",
+      desc: "Voluptate eum rerum deleniti reiciendis laborum dignissimos nisi tempore repudiandae, quaerat commodi expedita nobis",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1bx2TUoCGN0KneTn5BBciK4_A7MVvm3k7/view?usp=sharing",
+  socialLinks: {
+    devto: "https://dev.to/nnajiforemma10",
+    twitter: "https://twitter.com/NnajioforEmma10",
+    linkedin: "https://www.linkedin.com/in/emmanuel-nnajiofor-94b913193/",
+    github: "https://github.com/Emiklad24",
+    facebook: "https://web.facebook.com/emik.lad.3",
+  },
+};
+
+export default userData;
---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import ContainerBlock from "../components/ContainerBlock";
+import FavouriteProjects from "../components/FavouriteProjects";
+import LatestCode from "../components/LatestCode";
+import Hero from "../components/Hero";
+import getLatestRepos from "@lib/getLatestRepos";
+import userData from "@constants/data";
+
+export default function Home({ repositories }) {
+  return (
+    <ContainerBlock
+      title="Emmanuel Nnajiofor - Developer, Writer, Creator"
+      description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
+    >
+      <Hero />
+      <FavouriteProjects />
+      <LatestCode repositories={repositories} />
+    </ContainerBlock>
+  );
+}
+
+export const getServerSideProps = async () => {
+  let token = process.env.GITHUB_AUTH_TOKEN;
+
+  const repositories = await getLatestRepos(userData, token);
+
+  return {
+    props: {
+      repositories,
+    },
+  };
+};
