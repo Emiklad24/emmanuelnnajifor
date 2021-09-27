029d73320eb4a8710dc4491d640560d05aba5bdf
EmmanuelChinonye Nnajiofor <emiklad1993@gmail.com>
2021-09-27 19:04:36 +0100

    added service workers

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
-const withImages = require("next-images");
-module.exports = withImages();
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  webpack5: true,
+  pwa: {
+    dest: "public",
+  },
+});
