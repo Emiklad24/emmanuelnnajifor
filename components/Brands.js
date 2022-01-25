7cc43eb08c177d237517d165a611f267d88e7017
EmmanuelChinonye Nnajiofor <emiklad1993@gmail.com>
2022-01-26 00:03:34 +0100

    minor work on brands sections

--- components/Brands.js
+++ components/Brands.js
@@ -5,13 +5,13 @@ export const Brands = () => {
     <div className="container mx-auto pt-16 pb-40">
       <div className="w-11/12 xl:w-2/3 lg:w-2/3 md:w-2/3 mx-auto sm:mb-10 mb-16">
         <h1 className=" xl:text-5xl md:text-3xl text-xl text-center text-gray-800 font-extrabold mb-5 pt-4 dark:text-gray-100">
-          Partnerships with Coveted Brands
+          Clients & Partners
         </h1>
-        <p className="text-base md:text-lg lg:text-xl text-center text-gray-600 font-normal xl:w-10/12 xl:mx-auto dark:text-gray-100">
+        {/* <p className="text-base md:text-lg lg:text-xl text-center text-gray-600 font-normal xl:w-10/12 xl:mx-auto dark:text-gray-100">
           Our success has come from being committed to the property and
           investing in the development of the product to maximize sales. At the
           same time and maintaining the integrity.
-        </p>
+        </p> */}
       </div>
       <div className="xl:py-16 lg:py-16 md:py-16 sm:py-16 px-15 flex flex-wrap">
         <div
@@ -52,16 +52,15 @@ export const Brands = () => {
         </div>
         <div
           className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:pt-10 lg:pt-10 md:pt-2 pt-16"
-          title="borrowlite-logo"
+          title="Borrowlite"
         >
-          <img src="/borrowlite.png" alt width="200" />
+          <img src="/borrowlite.png" alt="borrowlite-logo" width="200" />
         </div>
-        <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-l lg:border-l border-gray-200 xl:pt-10 lg:pt-10 md:pt-2 pt-16">
-          <img
-            src="https://cdn.tuk.dev/assets/honda-dark.png"
-            alt
-            width="170"
-          />
+        <div
+          className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-l lg:border-l border-gray-200 xl:pt-10 lg:pt-10 md:pt-2 pt-16"
+          title="VKM"
+        >
+          <img src="vkm-logo.png" alt="vkm-logo" width="170" />
         </div>
       </div>
     </div>
